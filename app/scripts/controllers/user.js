dd062dd59c1fa2f8c74f778c7332e0104a048598
Deon <deon@dataprime.co.za>
2016-10-14 08:22:53 +0200

    First Commit

---
+++ app/scripts/controllers/user.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name myAppApp.controller:UserCtrl
+ * @description
+ * # UserCtrl
+ * Controller of the myAppApp
+ */
+angular.module('myAppApp')
+  .controller('UserCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
